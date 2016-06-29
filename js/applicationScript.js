24fd546420f7393b6a44ced32640e875e76d77b7
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-29 21:10:20 +0200

    ApplicationScript edited by Thomas Winkler

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -33,7 +33,7 @@
 var client;
 
 var init = function() {
-  
+  openImage("{}")
   var iwcCallback = function(intent) {
     // define your reactions on incoming iwc events here 
     console.log(intent);
