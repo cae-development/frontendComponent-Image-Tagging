f30374ed44a823f974b0d66b2cb1f322afe52189
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-07-02 21:44:50 +0200

    Function[openImage] edited by Thomas Winkler

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -78,7 +78,8 @@ var openImage = function(imageJsonString){
 
   $("#imageContainer").html(imageElement);
   //Additional own javascript 
-  loadComments(imageJson.id);
+  loadComments(imageJson.id); 
+  loadT
 
 }
 
