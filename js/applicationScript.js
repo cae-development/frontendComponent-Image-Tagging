167fdd435f444a2241624968057a27d620b5577b
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-07-02 21:43:17 +0200

    MicroserviceCall edited by Thomas Winkler

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -92,7 +92,7 @@ var loadTags = function(){
   client.sendRequest("GET", "tagging/tags/"+currentImg, "", "", {}, false,
   function(data, type) {
     console.log(data);
-    var tags = $("<h5/>").append(data.tags.map( function(tag){ 
+    var tags = $("<h4/>").append(data.tags.map( function(tag){ 
         return $("<span class='label label-primary'/>").html(tag.name); 
     }));
     $("#tagsContainer").html(tags);
