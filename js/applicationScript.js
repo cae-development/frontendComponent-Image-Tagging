4508f9d3a00f93dfbb53019dd86a5311a45571c7
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-07-03 14:03:49 +0200

    Function[openImage] edited by Thomas Winkler

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -65,7 +65,7 @@ var openImage = function(imageJsonString){
 
 //start variable declaration
   var imageJson = JSON.parse(imageJsonString); 
-  var image = $("<img/") 
+  var image = $("<img/").setAttribute("src",imageJson.url); 
 //end variable declaration
 
 
