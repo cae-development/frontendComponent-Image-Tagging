9945ac09c13514eb1c372aa3caa7698d358a15f1
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-07-06 15:20:54 +0200

    Code regeneration/Model synchronization

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -42,7 +42,7 @@ var init = function() {
     }
   };
 
-  client = new Las2peerWidgetLibrary("http://localhost:8086", iwcCallback);
+  client = new Las2peerWidgetLibrary("http://134.61.172.116:8086", iwcCallback);
 
   $('#addTag').on('click', function() {
     //start parameter initiation
